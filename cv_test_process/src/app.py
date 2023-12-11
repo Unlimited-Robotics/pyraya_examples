ec255479e7362ae61af090d77201001ade8f7829
Bryan Betancur <bryan@unlimited-robotics.com>
2023-12-11 10:33:47 -0500

    [cv] updated test_process example to check enabling and disabling times

--- cv_test_process/src/app.py
+++ cv_test_process/src/app.py
@@ -4,17 +4,19 @@
 from raya.application_base import RayaApplicationBase
 from raya.controllers.cameras_controller import CamerasController
 from raya.controllers.cv_controller import CVController
+import time
 
-
-CAMERA = 'nav_top'
+CAMERA = 'nav_bottom'
 DELAY = 0.3
 PREDICTION_TIMEOUT = 5.0
-MODEL_NAME = 'yunet_face'
+MODEL_NAME = 'apriltags'
+
 
 
 class RayaApplication(RayaApplicationBase):
 
     async def setup(self):
+        self.log.info('Ra-Ya Py - Computer Vision')
         self.cameras: CamerasController = \
                 await self.enable_controller('cameras')
         await self.cameras.enable_camera(CAMERA)
@@ -28,11 +30,13 @@ class RayaApplication(RayaApplicationBase):
         self.tries += 1
         self.log.info('')
         self.log.info(f'Enabling model, try {self.tries}...')
+        start_time = time.time()
         self.model= await self.cv.enable_model(
                 name=MODEL_NAME, 
                 source=CAMERA,
                 model_params = self.model_params
             )
+        print('ENABLING TIME: ', time.time()-start_time)
         class_type = type(self.model).__name__
         if class_type in ['ObjectsDetectorHandler', 
                           'FacesDetectorHandler',
@@ -58,7 +62,6 @@ class RayaApplication(RayaApplicationBase):
                 )          
         self.log.info('Model enabled')
         await self.sleep(DELAY)
-
         self.log.info('Waiting for prediction...')
         counter = 0
         while not self.prediction_received:
@@ -75,11 +78,17 @@ class RayaApplication(RayaApplicationBase):
         await self.sleep(DELAY)
 
         self.log.info('Disabling model...')
+        start_time2 = time.time()
         await self.cv.disable_model(model_obj=self.model)
         self.log.info('Model disabled')
-
+        print('DISABLING TIME: ', time.time()-start_time2)
+        print('time enabling and disabling: ', time.time()-start_time)
         await self.sleep(1.0)
 
     
     def callback_all_prediction(self, _):
         self.prediction_received = True
+
+
+    async def finish(self):
+        self.log.info('')
