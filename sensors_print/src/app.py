c3200a70b532d1a47843d7b44cacfa2cf2f475e1
Camilo Camacho <camilo@unlimited-robotics.com>
2024-06-07 19:20:35 +0000

    refactor(Sensors Controller): New NoData exception

--- sensors_print/src/app.py
+++ sensors_print/src/app.py
@@ -2,6 +2,7 @@ import json
 
 from raya.application_base import RayaApplicationBase
 from raya.controllers.sensors_controller import SensorsController
+from raya.exceptions import RayaSensorsNoData
 
 
 class RayaApplication(RayaApplicationBase):
@@ -13,13 +14,16 @@ class RayaApplication(RayaApplicationBase):
 
 
     async def loop(self):
-        sensors_data = self.sensors.get_all_sensors_values()
         self.log.info('')
         self.log.info(f'----------------{self.i}--------------')
+        try:
+            sensors_data = self.sensors.get_all_sensors_values()
+        except RayaSensorsNoData:
+            self.log.info(f'Waiting for data from the robot\'s sensors...')
         self.log.info(json.dumps(sensors_data, indent=2))
         self.log.info(f'----------------{self.i}--------------')
-        await self.sleep(0.5)
         self.i += 1
+        await self.sleep(0.5)
 
         
     async def finish(self):
